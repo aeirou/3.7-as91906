245c60ae124d6e2c9c0d388b65f79307b3352a26
aeirou <aeironefelipee@gmail.com>
2024-03-10 21:42:07 +1300

    Add movement & finish map

--- as91906/TOR_2024.py
+++ as91906/TOR_2024.py
@@ -1,5 +1,3 @@
-from tkinter import tk
-
 #tutorial
 
 locs_list = [
@@ -15,20 +13,27 @@ locs_list = [
 }
 ]
 
+class Map:
+    def __init__(self):
+        self.map = [] 
+
+    def add_locs(self):
+        new_map = self.map.append(self.name)
+        return new_map
 class Location:
     def __init__(self,name,desc,next_loc=None): #next_loc is the node and is set to 'None' by default — node will not lead to anywhere by default.
         self.name = name 
         self.desc = desc
         self.next_loc = next_loc
-    
+
+    def __repr__(self): 
+        return f"{self.name} {self.desc} {self.next_loc}" 
+
 location = [Location(**locations) for locations in locs_list]
 
-class Map:
-    def __init__(self):
-        self.map = [] 
-    
-    def add_locs():
-        map.appen
+print(location)
+
+
 
 
 
--- as91906/code_notes.py
+++ as91906/code_notes.py
@@ -5,3 +5,11 @@
 #  "next_loc": A,B,C ==> self.next_loc = next_loc }
 #]
 # location = [Location(**locations) for locations in locs_list]
+
+
+#class Location:
+#location is the object which contains attributes and methods (defs')
+
+#this code turns object into readable lines.
+#def __repr__(self): 
+        # return f"{self.name} {self.desc} {self.next_loc}" 
